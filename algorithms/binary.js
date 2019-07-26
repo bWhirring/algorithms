244597b420116e62e19a2d09f0acee960a816978
huhu <bianlongting@duiba.com.cn>
2019-07-26 14:16:19 +0800

    add binary

---
+++ algorithms/binary.js
@@ -0,0 +1,25 @@
+/**
+ * 二分查找法
+ * @param {Array} arr 查找的字符串
+ * @param {*} num 查找的数字
+ * @returns
+ */
+function binary(arr, num) {
+  let high = arr.length;
+  let low = 0;
+
+  while (low <= high) {
+    const mid = Math.floor((high + low) / 2);
+    const guess = arr[mid];
+
+    if (guess === num) {
+      return mid;
+    } else if (guess > num) {
+      high = mid - 1;
+    } else if (guess < num) {
+      low = mid + 1;
+    } else {
+      return null;
+    }
+  }
+}
